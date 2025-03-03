af7e12d0689e19047d051e7030dcb50bd3f3d747
Hazem Ibrahim <141569852+Hazem191@users.noreply.github.com>
2025-03-03 23:33:54 +0200

    Hazem Ibrahim Abdel-Hamed Mohamed Aldriny >> Section:3
    Insertion Sort Algorithm

---
+++ Insetion Sort.py
@@ -0,0 +1,16 @@
+def Insrtion_Algothim(a):
+    for j in range(1, len(a)):
+        key = a[j]
+        i = j - 1
+        while i >= 0 and a[i] > key:
+            a[i + 1] = a[i]
+            i -= 1
+        a[i + 1] = key
+
+print("Name: Hazem Ibrahim Abdel-Hamed Mohamed Aldriny >> Section:3")
+print("---------------------------------")
+arr = [ 15, 10, 25, 20, 30, 40, 35]
+print("Original array:",arr)
+print("---------------------------------")
+Insrtion_Algothim(arr)
+print("Sorted array:",arr)
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        string Name = "Hazem Ibrahim Abdel-Hamed Mohamed Aldriny";
+        int Section = 3;
+        Console.WriteLine($"Name: {Name} >> Section:{Section}");
+        Console.WriteLine("------------------------------------");
+        Console.WriteLine("Insertion Sort Algorithm: Move elements that are greater than key one position ahead");
+      
+      
+        int[] arr = { 15, 10, 25, 20, 30, 40, 35 };
+
+        Console.WriteLine("------------------------------------");
+        Console.Write("Orginal array ");
+        printArray(arr);
+
+        Console.WriteLine("------------------------------------");
+        Console.Write("Sorted array ");
+        Insertion_Sort(arr);
+        printArray(arr);
+        Console.ReadLine();
+    }
+
+    static void Insertion_Sort(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j -= 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+
+    private static void printArray(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+
+            Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+}
